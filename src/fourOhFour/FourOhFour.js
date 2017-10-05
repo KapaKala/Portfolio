39aceeda5a043a544ea47b887715c91adba5bdb7
henri <henri.kankaanpaa@cs.tamk.fi>
2017-10-06 02:15:58 +0300

    Made 404 page that much cooler

--- src/fourOhFour/FourOhFour.js
+++ src/fourOhFour/FourOhFour.js
@@ -3,7 +3,14 @@ import React, {Component} from 'react';
 export default class FourOhFour extends Component {
     render() {
         return (
-            <h1 className="not-found" style={{fontSize: 300, margin: "auto"}}>404</h1>
+            <div className="four-oh-four-container"
+                 style={{backgroundImage: "url(" + require('../images/1401994195056.gif') + ")",
+                         backgroundSize: 'cover', backgroundPosition: '50% 70%', height: '100%',
+                         display: 'flex'}}>
+                <div style={{fontSize: 200, margin: 'auto'}}>
+                    <h1 className="not-found" style={{margin: 'auto', padding: 0}}>404</h1>
+                </div>
+            </div>
         )
     }
 }
