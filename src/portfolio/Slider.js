1f25ad037d71ad14e8213290b6cdc65eafc80f23
henri <henri.kankaanpaa@cs.tamk.fi>
2017-12-03 21:55:12 +0200

    Adjust image display

--- src/portfolio/Slider.js
+++ src/portfolio/Slider.js
@@ -9,63 +9,61 @@ const Slider = props => (
   <div
     className="img-div"
     style={{
-      display: 'flex',
       width: props.width,
       height: props.height,
     }}
   >
-    <div className="window-titlebar" style={{ width: props.width, height: 12 }}>
-      <span className="titlebar-button" style={{ height: 14, width: 14, marginLeft: 14 }} />
-      <span className="titlebar-button" style={{ height: 14, width: 14 }} />
-      <span className="titlebar-button" style={{ height: 14, width: 14 }} />
-    </div>
-    <div
-      className="img-loader"
-      style={
-        props.display ? { opacity: 0, visibility: 'hidden' } : { opacity: 1, visibility: 'visible' }
-      }
-    >
-      <Spinner name="double-bounce" />
-      <h3>loading</h3>
-    </div>
-    <div className="img-wrapper" style={{ transform: `translateX(${props.translateValue}px)` }}>
-      {props.createImages()}
-    </div>
-
-    <div className="img-controls" style={{ width: props.width, height: props.height }}>
+    <div className="slider-img-container">
       <div
-        className="prev-img"
-        onClick={() => {
-          props.prevImg();
-        }}
-        onKeyDown={(e) => {
-          if (e.key === 'ArrowLeft') {
-            props.prevImg();
-          }
-        }}
-        role="button"
-        tabIndex="0"
+        className="img-loader"
+        style={
+          props.display
+            ? { opacity: 0, visibility: 'hidden' }
+            : { opacity: 1, visibility: 'visible' }
+        }
       >
-        <span className="control">
-          <FontAwesome name="angle-left" size="3x" />
-        </span>
+        <Spinner name="double-bounce" />
+        <h3>loading</h3>
       </div>
-      <div
-        className="next-img"
-        onClick={() => {
-          props.nextImg();
-        }}
-        onKeyDown={(e) => {
-          if (e.key === 'ArrowRight') {
+      <div className="img-wrapper" style={{ transform: `translateX(${props.translateValue}px)` }}>
+        {props.createImages()}
+      </div>
+
+      <div className="img-controls" style={{ width: props.width, height: props.height }}>
+        <div
+          className="prev-img"
+          onClick={() => {
+            props.prevImg();
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'ArrowLeft') {
+              props.prevImg();
+            }
+          }}
+          role="button"
+          tabIndex="0"
+        >
+          <span className="control">
+            <FontAwesome name="angle-left" size="3x" />
+          </span>
+        </div>
+        <div
+          className="next-img"
+          onClick={() => {
             props.nextImg();
-          }
-        }}
-        role="button"
-        tabIndex="-1"
-      >
-        <span className="control">
-          <FontAwesome name="angle-right" size="3x" />
-        </span>
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'ArrowRight') {
+              props.nextImg();
+            }
+          }}
+          role="button"
+          tabIndex="-1"
+        >
+          <span className="control">
+            <FontAwesome name="angle-right" size="3x" />
+          </span>
+        </div>
       </div>
     </div>
   </div>
